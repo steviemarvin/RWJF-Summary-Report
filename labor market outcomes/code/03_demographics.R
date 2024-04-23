ee96538d876f9a914796730e4f0f97a0846e7968
steviemarvin <marvinstevie01@gmail.com>
2024-04-23 14:26:07 -0400

    deleted spouse locating stuff for marital stat

--- labor market outcomes/code/03_demographics.R
+++ labor market outcomes/code/03_demographics.R
@@ -6,14 +6,16 @@ demo_2022 <- filter(asec_clean, year == 2022) %>%
          marst, nchild, pernum, pov100, pov50, relate, related_adult, sex, sploc, contains("wbhaa"), yngch, u18, u6) %>% 
   mutate(childu18 = if_else(age < 18, 1, 0),
          childu6 = if_else(age < 6, 1, 0)) %>% 
-  inner_join(li_families, by = c("year", "serial"))
+  inner_join(li_families, by = c("year", "serial")) %>% 
+  filter(hh_type == "hhOnly" | hh_type == "hhXnr" | hh_type == "hhXr" | hh_type == "hhXnrXr")
+
 
 #### TYPE OF LOW INCOME FAMILIES BY 7 HOUSEHOLD TYPES #### 
 li_hh_type <- li_families %>% 
   group_by(year, hh_type) %>% 
   summarize(n = n(),
             wsum = sum(hhwgt)) %>% 
-  pivot_wider(id_cols = "year", values_from = c("n", "wsum"), names_from = "hh_type", names_sep = "_")
+  pivot_wider(id_cols = year, values_from = c("n", "wsum"), names_from = "hh_type", names_sep = "_")
 
 
 #### SHARE OF LOWINCOME FAMILIES BY WBHAA ####
@@ -82,8 +84,14 @@ li_hh_wbhaa <- demo_2022 %>%
 #### SHARE OF LOW INCOME FAMILIES THAT CAN BE CLASSIFIED AS NATIVE- OR FOREIGN-BORN ####
 li_hh_foreignborn <- demo_2022 %>% 
   filter(relate == 101) %>% 
-  summarize(foreign_born_s = sum(foreign_born * hhwgt),
-            foreign_born_p = weighted.mean(foreign_born, w = hhwgt))
+  summarize(foreign_born_s = sum(foreign_born * hhwgt, na.rm = TRUE),
+            foreign_born_p = weighted.mean(foreign_born, w = hhwgt, na.rm = TRUE))
+
+li_foreignbornXwbhaa <- demo_2022 %>% 
+  filter(relate == 101) %>% 
+  group_by(wbhaa) %>% 
+  summarize(foreign_born_s = sum(foreign_born * hhwgt, na.rm = TRUE),
+            foreign_born_p = weighted.mean(foreign_born, w = hhwgt, na.rm = TRUE))
 
 #### NUMBER OF CHILDREN UNDER 18 (AND UNDER 6) IN THE HOUSEHOLD ####
 li_hh_child <- demo_2022 %>% 
@@ -124,147 +132,28 @@ li_hh_childXwbhaa <- demo_2022 %>%
 
 #### MARITAL STATUS AND LGB STATUS OF PARENTS OF CHILDREN UNDER 18 ####
 ## Marital status of parents (married or not) i.e. married families vs non married
-# gathers LGB status and marital status of all household heads and their spouse/partner
-parents_marst_sploc0 <- demo_2022 %>% 
-  filter(u18 == 1, sploc == 0) %>% 
-  mutate(across(relate | marst, ~as.character(as_factor(.x)))) %>% 
-  group_by(serial, wbhaa, relate, marst) %>% 
-  reframe(n = n(),
-          hhwgt = max(asecwth),
-          pwgt = asecwt, 
-          famrel = famrel) %>% ungroup() %>% 
-  mutate(not_married = if_else(marst != "Married, spouse present" & marst != "Married, spouse absent", 1, 0)) %>% 
-  group_by(wbhaa, not_married) %>% 
-  summarize(uw = n(),
-            w_ct = sum(pwgt))
-
-parents_marst_sploc <- demo_2022 %>% 
-  filter(u18 == 1, sploc != 0) %>% 
-  mutate(across(relate | marst, ~as.character(as_factor(.x)))) %>% 
-  group_by(serial, wbhaa, relate, marst) %>% 
-  reframe(n = n(),
-          famrel = famrel,
-          hhwgt = max(asecwth),
-          pwgt = asecwt,
-          hh_type = hh_type) %>% ungroup() %>% 
-  filter(relate != "Spouse", relate != "Unmarried partner", famrel != 2) %>% 
-  mutate(ID = as.character(rowid(serial)),
-         not_married = if_else(marst != "Married, spouse present" & marst != "Married, spouse absent", 1, 0)) %>% 
-  group_by(wbhaa, not_married) %>% 
-  summarize(uw = n(),
-            w_ct = sum(pwgt))
-
-# combined sums of parent sets by wbhaa and whether they are married or not
-parents_marst <- full_join(parents_marst_sploc0, parents_marst_sploc, by = c("wbhaa", "not_married")) %>% 
-  mutate(uw_ct = uw.x + uw.y,
-         w_ct = round(w_ct.x + w_ct.y, 0)) %>% 
-  select(wbhaa, not_married, uw_ct, w_ct) %>% 
-  pivot_wider(id_cols = wbhaa, names_from = not_married, values_from = c("uw_ct", "w_ct"))
-
-parents_marst_person <- demo_2022 %>% 
-  filter(u18 == 1) %>% 
-  mutate(marst = as.character(as_factor(marst))) %>% 
-  group_by(wbhaa, marst) %>% 
-  summarize(uw = n(),
-            w = round(sum(asecwt), 0)) %>% 
-  pivot_wider(id_cols = wbhaa, names_from = marst, values_from = c("uw", "w"))
-
-parents_marst_person2 <- demo_2022 %>% 
-  filter(u18 == 1) %>% 
-  mutate(married_parent = labelled(married_parent, c("married" = 1, "not_married" = 0))) %>% 
+marst_hhhead <- demo_2022 %>% 
+  filter(u18 == 1 & relate == 101) %>% 
   group_by(wbhaa, married_parent) %>% 
   summarize(uw = n(),
-            w = round(sum(asecwt), 0)) %>% 
+            w = sum(hhwgt)) %>% 
   pivot_wider(id_cols = wbhaa, names_from = married_parent, values_from = c("uw", "w"))
 
-
-## LGB STUFF <3
-marst_LGB_hh <- demo_2022 %>% 
-  filter(hh_type == "hhOnly" | hh_type == "hhXr" | hh_type == "hhXnr" | hh_type == "hhXnrXr", hh_head_sp == 1 | relate == 1114) %>%
-  mutate(across(relate | marst, ~as.character(as_factor(.x)))) %>% 
-  group_by(serial, relate) %>% 
-  summarize(LGB = max(LGB), # LGB variable is person based not household based i.e. LGB == 1 if person is same-sex spouse/partner
-            marst = marst,
-            sex = sex,
-            wbhaa = wbhaa,
-            hhwgt = max(asecwth),
-            pwgt = max(asecwt)) %>% ungroup()
-
-
-# respondents with spouse present in the household
-# typically they have the same famrel title as the spouse or one of them is designated as "other rel. n.s."
-sploc_yes <- demo_2022 %>% 
-  filter(u18 == 1, (hh_type != "hhOnly"), sploc != 0, relate != 101, relate != 201, relate != 1114) %>% 
-  mutate(across(relate | marst, ~as.character(as_factor(.x)))) %>% 
-  group_by(serial, relate) %>% 
-  reframe(n = n(),
-          pernum = pernum,
-          sploc = sploc,
-          sex = sex,
-          marst = marst,
-          wbhaa = wbhaa,
-          hh_type = hh_type) %>% ungroup()
-
-# LGB status of parents with spouse present, and they have the same famrel title, hence 2 
-LGB_other_2 <- filter(sploc_yes, n == 2) %>% 
-  select(-n) %>% 
-  mutate(relate = if_else(relate == "Other relatives, n.s.", "oth_rel", relate)) %>% 
-  group_by(serial) %>% 
-  mutate(ID = as.character(rowid(serial)),
-         relate_ID = paste(relate, ID, sep = "_")) %>% ungroup() %>% 
-  pivot_wider(id_cols = serial, names_from = relate_ID, values_from = c("pernum", "sploc", "sex", "wbhaa")) %>% 
-  mutate(spouse_check = case_when(!is.na(pernum_Parent_1) & pernum_Parent_1 == sploc_Parent_2 ~ 1,
-                                  !is.na(pernum_oth_rel_1) & pernum_oth_rel_1 == sploc_oth_rel_2 ~ 1, 
-                                  !is.na(`pernum_Housemate/roomate_1`) & `pernum_Housemate/roomate_1` == `sploc_Housemate/roomate_2` ~ 1,
-                                  !is.na(`pernum_Roomer/boarder/lodger_1`) & `pernum_Roomer/boarder/lodger_1` == `sploc_Roomer/boarder/lodger_2` ~ 1,
-                                  !is.na(`pernum_Other nonrelatives_1`) & `pernum_Other nonrelatives_1` == `sploc_Other nonrelatives_2` ~ 1),
-         LGB = if_else((!is.na(sex_Parent_1) & sex_Parent_1 == sex_Parent_2 )| (!is.na(sex_oth_rel_1) & sex_oth_rel_1 == sex_oth_rel_2) | 
-                        (!is.na(`sex_Housemate/roomate_1`) & `sex_Housemate/roomate_1` == `sex_Housemate/roomate_2`) |
-                        (!is.na(`sex_Roomer/boarder/lodger_1`) & `sex_Roomer/boarder/lodger_1` == `sex_Roomer/boarder/lodger_2`) |
-                         (!is.na(`sex_Other nonrelatives_1`) & `sex_Other nonrelatives_1` == `sex_Other nonrelatives_2`), 1, 0)) %>% 
-  filter(spouse_check == 1, LGB == 1) %>% 
-  select(serial, spouse_check, LGB) %>% 
-  left_join(demo_2022, by = "serial", multiple = "all") %>% 
-  filter(u18 == 1, hh_head_sp == 0, relate != 1114) %>%  select(serial, relate, marst, sex, wbhaa, pwgt = asecwt, hhwgt = asecwth) %>% 
-  mutate(across(relate | marst | sex, ~as.character(as_factor(.x))))
-
-# LGB status of parents with spouse present, and they have different famrel title, hence 1
-LGB_other_1 <- filter(sploc_yes, n == 1) %>% 
-  select(-n) %>% 
-  mutate(relate = if_else(relate == "Other relatives, n.s.", "oth_rel", relate)) %>%
-  group_by(serial) %>% 
-  pivot_wider(id_cols = serial, names_from = relate, values_from = c("pernum", "sploc", "sex")) %>% 
-  mutate(spouse_check = if_else((!is.na(pernum_Child) | !is.na(pernum_Sibling) | !is.na(pernum_Grandchild)) &
-                                  pernum_oth_rel == sploc_Child | pernum_oth_rel == sploc_Sibling | pernum_oth_rel == sploc_Grandchild, 1, 0),
-         LGB = if_else((!is.na(pernum_Child) | !is.na(pernum_Sibling) | !is.na(pernum_Grandchild)) &
-                         (sex_oth_rel == sex_Child | sex_oth_rel == sex_Sibling | sex_oth_rel == sex_Grandchild), 1, 0)) %>% 
-  filter(spouse_check == 1, LGB == 1) %>% 
-  select(serial, spouse_check, LGB) %>% 
-  left_join(demo_2022, by = "serial", multiple = "all") %>% 
-  filter(u18 == 1, hh_head_sp == 0, relate != 1114) %>%  select(serial, relate, marst, sex, wbhaa, pwgt = asecwt, hhwgt = asecwth) %>% 
-  mutate(across(relate | marst | sex, ~as.character(as_factor(.x))))
-
-
-## getting sums of marital status and LGB status
 # LGB status of household head (that has own child) by wbhaa
 LGB_parents <- marst_LGB_hh %>%
   filter(LGB == 1) %>% select(-wbhaa) %>% 
   left_join(hh_head_wbhaa, by = "serial") %>% 
-  bind_rows(list(LGB_other_1, LGB_other_2)) %>% 
-  group_by(serial) %>% 
-  mutate(ID = as.character(rowid(serial)),
-         LGB = 1) %>% 
-  filter(ID == 1) %>% 
-  group_by(wbhaa_hh, sex) %>% 
+  group_by(wbhaa_hh) %>% 
   summarize(
     LGB_uw = sum(LGB),
-    LGB = round(sum(pwgt), 0)) %>% 
+    LGB = round(sum(hhwgt), 0)) %>% 
   filter(!is.na(wbhaa_hh))
-  
+
+
 
 #### AGE OF PARENTS ####
 parents_age <- demo_2022 %>% 
-  filter(u18 == 1) %>% 
+  filter(u18 == 1, relate == 101) %>% 
   group_by(wbhaa, sex) %>% 
   summarize(mean = round(weighted.mean(age, w = asecwt), 1), 
             median = median(age)) %>% 
@@ -272,33 +161,15 @@ parents_age <- demo_2022 %>%
 
 
 #### DISABILITY STATUS OF PARENT OR HOUSEHOLD MEMBER ####
-disabled_members <- demo_2022 %>%
-  filter(diffany == 2) %>% 
-  mutate(relate = as.character(as_factor(relate))) %>% 
-  group_by(serial, relate) %>% 
-  summarize(n = n()) %>% 
-  pivot_wider(id_cols = serial, names_from = relate, values_from = "n") %>% 
-  inner_join(filter(families_hh_type, year == 2022), by = "serial") %>% select(-year) 
-
-li_hh_disability <- demo_2022 %>% 
-  mutate(diffany_ind = case_when(diffany == 2 ~ 1,
-                                 diffany == 1 ~ 0,
-                                 diffany == 0 ~ NA)) %>% 
-  group_by(serial) %>% 
-  summarize(has_diffany = max(diffany_ind, na.rm = TRUE),
-            hhwgt = max(asecwth)) %>% 
-  summarize(has_diffany = weighted.mean(has_diffany, w = hhwgt))
-
-
 li_hh_disabilityXwbhaa <- demo_2022 %>% 
-  mutate(diff_parent = if_else(u18 == 1 & diffany == 2, 1, 0),
-         diff_member = if_else(u18 == 0 & diffany == 2, 1, 0)) %>% 
+  mutate(diff_parent = if_else((hh_head_sp == 1 | relate == 1114) & diffany == 2, 1, 0),
+         diff_member = if_else((hh_head_sp == 0 & relate != 1114) & diffany == 2, 1, 0)) %>% 
   group_by(serial) %>%
   summarize(diff_parent = max(diff_parent),
             diff_member = max(diff_member),
             hhwgt = max(asecwth)) %>% ungroup() %>% 
   mutate(parentXmember = if_else(diff_parent > 0 & diff_member > 0, 1, 0),
-         parentormember = if_else(diff_parent >0 | diff_member > 0, 1, 0)) %>% 
+         parentormember = if_else(diff_parent > 0 | diff_member > 0, 1, 0)) %>% 
   left_join(hh_head_wbhaa, by = "serial") %>% 
   group_by(wbhaa_hh) %>% 
   summarize(across(diff_parent | diff_member | parentXmember | parentormember, ~weighted.mean(.x, w = hhwgt), .names = "%.{.col}"),
@@ -309,7 +180,6 @@ li_hh_disabilityXwbhaa <- demo_2022 %>%
 #### INTERGENERATIONAL HOUSEHOLDS ####
 # baseline df that classifies households as intergenerational or not, based on related members present in the household
 intergen_hh <- demo_2022 %>% 
-  filter(hh_type != "nrXr" & hh_type != "nrOnly") %>% 
   mutate(relate = as.character(as_factor(relate))) %>% 
   group_by(serial, relate) %>% 
   summarize(n = n()) %>% 
@@ -342,7 +212,7 @@ foster_hh <- demo_2022 %>%
   left_join(demo_2022, by = c("year", "serial"), multiple = "all")
 
 
-### SPM vs FPL ###
+#### SPM vs FPL ####
 poverty_hh <- asec_clean %>%
   inner_join(families_hh_type, by = c("year", "serial"), multiple = "all") %>%
   filter(relate == 101, hh_type == "hhOnly" | hh_type == "hhXr" | hh_type == "hhXnr" | hh_type == "hhXnrXr") %>% 
